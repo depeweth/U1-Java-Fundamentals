ffeb108dc5903cabd147dc4b97f020dfed5edca8
depeweth000 <depeweth000@warren.k12.in.us>
2021-08-04 11:51:19 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,20 @@
 public class L2 {
 
     //add the main method header
+    public static void main(String[] args){
+        //values
+        String firstName = "George Frank";
+        int age = 16;
+        double balance = 350.27;
+        int accountId = 4850024;
+        //On ATM screen
+        System.out.print("Hello:");
+        System.out.println(firstName);
+        System.out.print("Your:");
+        System.out.print(age);
+        System.out.println(" years old with");
+        System.out.print("$");
+        System.out.println(balance);
+        System.out.print(accountId);
+    }
 }
